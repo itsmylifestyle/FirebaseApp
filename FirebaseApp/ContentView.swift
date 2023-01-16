8957a65dd0d211e715638a942f8aa7cbe9263b07
itsmylifestyle <hahaii00@mail.ru>
2023-01-16 22:38:19 +0600

    Initial Commit

---
+++ FirebaseApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseApp
+//
+//  Created by Айбек on 16.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirebaseApp/FirebaseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAppApp.swift
+//  FirebaseApp
+//
+//  Created by Айбек on 16.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
